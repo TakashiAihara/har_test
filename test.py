a7dd590f7fb07027b1b96d07053c6f8577366e68
takashi <takashi.aihara.0523@gmail.com>
2020-05-17 16:36:51 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,35 @@
+from browsermobproxy import Server
+from selenium import webdriver
+
+server = Server('path_to_executable')
+server.start()
+proxy = server.create_proxy()
+profile  = webdriver.FirefoxProfile()
+profile.set_proxy(proxy.selenium_proxy())
+driver = webdriver.Firefox(firefox_profile=profile)
+proxy.new_har("file_name", options={'captureHeaders': True, 'captureContent': True})
+driver.get("your_url")
+proxy.wait_for_traffic_to_stop(1, 60)
+for ent in proxy.har['log']['entries']:
+    print(ent)
+
+server.stop()
+driver.quit()
+
+from browsermobproxy import Server
+server = Server("path/to/browsermob-proxy")
+server.start()
+proxy = server.create_proxy()
+
+from selenium import webdriver
+profile  = webdriver.FirefoxProfile()
+profile.set_proxy(proxy.selenium_proxy())
+driver = webdriver.Firefox(firefox_profile=profile)
+
+
+proxy.new_har("google")
+driver.get("http://www.google.co.uk")
+proxy.har # returns a HAR JSON blob
+
+server.stop()
+driver.quit()
